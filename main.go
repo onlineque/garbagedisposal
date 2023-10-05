ebbafe3a15963fe2708f1f565ba28dd1182b9254
Vladimir Siman <online@clew.cz>
2023-10-05 16:11:37 +0200

    list all Succeeded pods

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error initializing API:", err)
 	}
-	err = k8sfunctions.GetPods(clientset, "", "Completed")
+	err = k8sfunctions.GetPods(clientset, "", "Succeeded")
 	if err != nil {
 		log.Fatal(err)
 	}
